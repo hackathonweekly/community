2f34f80c330011f3ed5e08be7bf5983bffd150bb
jackiexiao <707610215@qq.com>
2025-10-29 13:04:35 +0800

    chore: 移除不再需要的 ignore-punycode-warning 依赖
    不再需要 ignore-punycode-warning 包来抑制警告，改为使用 NODE_NO_WARNINGS 环境变量

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,3 @@
-import "ignore-punycode-warning";
 import { withContentCollections } from "@content-collections/next";
 import type { NextConfig } from "next";
 import nextIntlPlugin from "next-intl/plugin";
